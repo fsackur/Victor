0e37a817364b409d1b6d563c0563248ca9f93952
Freddie Sackur <github@dustyfox.uk>
2021-08-17 23:15:02 +0100

    create module

---
+++ Victor.psm1
@@ -0,0 +1,3 @@
+
+Get-ChildItem $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue | ForEach-Object {. $_.FullName}
+Get-ChildItem $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue | ForEach-Object {. $_.FullName}
