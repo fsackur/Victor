fc278d61239299514f4dbcefe380c2ed48481928
Freddie Sackur <github@dustyfox.uk>
2021-08-18 00:34:23 +0100

    sample git repo for testing

--- Tests/Get-GitLog.Tests.ps1
+++ Tests/Get-GitLog.Tests.ps1
@@ -7,7 +7,22 @@ Describe Get-GitLog {
             Join-Path -ChildPath Victor.psd1 |
             Import-Module -PassThru -ErrorAction Stop
 
-        Push-Location $Module.ModuleBase
+
+        $TestPath = ($env:TEMP | Resolve-Path -ErrorAction SilentlyContinue), '/tmp' |
+            Select-Object -First 1 |
+            Join-Path -ChildPath 'Victor.Tests'
+        $TestPath = New-Item $TestPath -ItemType Directory -Force -ErrorAction Stop
+
+        $TestRepos = $PSScriptRoot |
+            Join-Path -ChildPath 'Data' |
+            Get-ChildItem -Filter 'repo*.zip' -PipelineVariable Path |
+            ForEach-Object {
+                $Destination = Join-Path $TestPath $_.BaseName
+                Expand-Archive $_.FullName $Destination -ErrorAction SilentlyContinue
+                Get-Item $Destination
+            }
+
+        Push-Location $TestRepos[0]
     }
 
     AfterAll {
@@ -15,6 +30,6 @@ Describe Get-GitLog {
     }
 
     It "Gets commits" {
-        (Get-GitLog).Count | Should -BeGreaterThan 2
+        (Get-GitLog).Count | Should -Be 6
     }
 }
