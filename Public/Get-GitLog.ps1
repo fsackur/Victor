729eaa54c7a75085893015b3bd431cb2cf728854
Freddie Sackur <github@dustyfox.uk>
2021-08-17 23:31:50 +0100

    add Get-GitLog

---
+++ Public/Get-GitLog.ps1
@@ -0,0 +1,29 @@
+using namespace System.Collections.Generic
+
+function Get-GitLog
+{
+    [CmdletBinding()]
+    param ()
+
+
+    $LogArgs = [List[string]]::new()
+
+    # https://git-scm.com/docs/git-log#_pretty_formats
+    $Format = [ordered]@{
+        Id        = '%h'
+        Author    = '%an'
+        UpdatedAt = '%ar'
+        Summary   = '%s'
+    }
+    $OutputProperties = @($Format.Keys)
+
+    $Delim        = [char]31    # non-printing char that we don't expect to find in git output
+    $FormatString = $Format.Values -join $Delim
+    $LogArgs.Add("--pretty=format:$FormatString")
+
+
+    # Do the thing
+    $CommitLines = & git log $LogArgs
+
+    $CommitLines | ConvertFrom-Csv -Delimiter $Delim -Header $OutputProperties
+}
