917ddd661e1d4ba9f5afa277e3504fdc05b5ed9d
Freddie Sackur <github@dustyfox.uk>
2021-11-20 19:42:24 +0000

    Don't keep zipped test repos, create from scratch

---
+++ Tests/Test.Setup.ps1
@@ -0,0 +1,58 @@
+
+Get-Module Victor | Remove-Module -Force -ErrorAction SilentlyContinue
+$Module = $PSScriptRoot |
+    Split-Path |
+    Join-Path -ChildPath Victor.psd1 |
+    Import-Module -PassThru -ErrorAction Stop
+
+function New-TestGitDir
+{
+    [CmdletBinding()]
+    param ()
+
+    $ErrorActionPreference = 'Stop'
+
+    if (-not $Global:__VICTOR_TEST_GIT_DIRS)
+    {
+        $Global:__VICTOR_TEST_GIT_DIRS = @()
+    }
+
+    $Temp = [IO.Path]::GetTempPath()
+    $Name = [IO.Path]::GetRandomFileName()
+    $Path = Join-Path $Temp $Name
+
+    New-Item $Path -ItemType Directory | Push-Location
+
+    $Global:__VICTOR_TEST_GIT_DIRS += $Path
+
+    try
+    {
+        git init *>&1 | Write-Debug
+        git checkout -b main *>&1 | Write-Debug
+        $Path
+    }
+    finally
+    {
+        Pop-Location
+    }
+}
+
+
+function Clear-TestGitDir
+{
+    [CmdletBinding()]
+    param ()
+
+    $ErrorActionPreference = 'Stop'
+
+    $_PWD = $PWD -replace '^Microsoft.PowerShell.Core\\FileSystem::'
+    if ($_PWD -in $Global:__VICTOR_TEST_GIT_DIRS)
+    {
+        Write-Warning "Not cleaning current location '$_PWD'."
+    }
+
+    @($Global:__VICTOR_TEST_GIT_DIRS) -ne $_PWD | Remove-Item -Recurse -Force
+    $Global:__VICTOR_TEST_GIT_DIRS = $Global:__VICTOR_TEST_GIT_DIRS |
+        Resolve-Path -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path
+}
