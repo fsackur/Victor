b185849614d86ada070ba959ba920e9abc807665
Freddie Sackur <github@dustyfox.uk>
2022-01-21 14:14:35 +0000

    Add Add-GitFile

--- Victor.psm1
+++ Victor.psm1
@@ -2,6 +2,7 @@
 Get-ChildItem $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue | ForEach-Object {. $_.FullName}
 Get-ChildItem $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue | ForEach-Object {. $_.FullName}
 
+Set-Alias a Add-GitFile
 Set-Alias amend Update-GitCommit
 Set-Alias ggl Get-GitLog
 Set-Alias rebase Invoke-GitRebase
