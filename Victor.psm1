b87c6bbbadd5094b3f9abaf3d2a3b4cb2fe5ae0e
Freddie Sackur <github@dustyfox.uk>
2022-01-21 10:55:23 +0000

    Add ggl and rst aliases

--- Victor.psm1
+++ Victor.psm1
@@ -1,3 +1,6 @@
 
 Get-ChildItem $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue | ForEach-Object {. $_.FullName}
 Get-ChildItem $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue | ForEach-Object {. $_.FullName}
+
+Set-Alias ggl Get-GitLog
+Set-Alias rst Reset-Git
