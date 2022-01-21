439908d7af38a544a0f8cc45d9bd0f544403f3d8
Freddie Sackur <github@dustyfox.uk>
2022-01-21 17:41:27 +0000

    ArgumentCompleter for rebase -Onto

--- Public/Invoke-GitRebase.ps1
+++ Public/Invoke-GitRebase.ps1
@@ -321,3 +321,12 @@ function Invoke-GitRebase
         }
     }
 }
+
+
+Register-ArgumentCompleter -CommandName Invoke-GitRebase -ParameterName Onto -ScriptBlock {
+    param ($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+
+    [string[]]$BranchNames = Get-GitBranch | Where-Object Active -ne $true | Select-Object -ExpandProperty Name
+
+    ($BranchNames -like "$wordToComplete*"), ($BranchNames -like "*?$wordToComplete*") | Write-Output
+}
