e8c67f768350c79a60ad77f4f0fd33f53eb463e0
Freddie Sackur <github@dustyfox.uk>
2022-01-21 12:28:31 +0000

    Get-GitLog supports -SinceLastMerge and -From

--- Public/Get-GitLog.ps1
+++ Public/Get-GitLog.ps1
@@ -2,17 +2,42 @@ using namespace System.Collections.Generic
 
 function Get-GitLog
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'SinceLastMerge')]
     param
     (
-        [int]$Count = 30
+        [Parameter(ParameterSetName = 'SinceLastMerge')]
+        [switch]$SinceLastMerge,
+
+        [Parameter(ParameterSetName = 'ByCount', Position = 0)]
+        [ValidateRange(1, 5000)]
+        [int]$Count = 30,
+
+        [Parameter(ParameterSetName = 'FromRef')]
+        [string]$From
     )
+
+    if ($PSCmdlet.ParameterSetName -eq 'SinceLastMerge')
+    {
+        $From = git log --merges -n 1 --format=%h
+
+        if (-not $From)
+        {
+            $From = git rev-list --max-parents=0 HEAD --abbrev-commit | Select-Object -First 1
+        }
+    }
 
 
     $LogArgs = [List[string]]::new()
     $LogArgs.Add("log")
 
-    $LogArgs.Add("-n $Count")
+    if ($From)
+    {
+        $LogArgs.Add("$From..HEAD")
+    }
+    else
+    {
+        $LogArgs.Add("-n $Count")
+    }
 
     # https://git-scm.com/docs/git-log#_pretty_formats
     $Format = [ordered]@{
