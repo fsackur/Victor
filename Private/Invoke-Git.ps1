8fd5f10a1215bc7efddc8c08a27cfd9bbffffbd4
Freddie Sackur <github@dustyfox.uk>
2022-02-20 17:16:39 +0000

    Better git errors

--- Private/Invoke-Git.ps1
+++ Private/Invoke-Git.ps1
@@ -2,21 +2,35 @@ using namespace System.Management.Automation
 
 function Invoke-Git
 {
-    "git $($args)" | Write-Verbose
+    $Invocation = "git $($args)"
+    $Invocation | Write-Verbose
 
     $Git = Get-Command git -CommandType Application
 
     $Output = & $Git @args *>&1
 
-    if ($LASTEXITCODE)
+    $HadError = [bool]$LASTEXITCODE
+
+    # ErrorRecords with empty Message cast to string as typename...
+    [string[]]$Output = $Output -replace '^System\.Management\.Automation\.RemoteException$'
+
+    if (-not $HadError)
     {
-        $Output = $Output | Out-String
-        $Output = $Output -replace '^[\s\r\n]*\n' -replace '\r?\n[\s\r\n]*$'
-        $Output | Write-Error
+        return $Output.PSObject.BaseObject
     }
-    else
-    {
-        $Output
-    }
+
+    $Output = $Output | Out-String
+    $Output = $Output -replace '^[\s\r\n]*\n' -replace '\r?\n[\s\r\n]*$'
+
+    $ErrorRecord = [ErrorRecord]::new(
+        [RuntimeException]::new($Output),
+        'NativeCommandError',
+        'FromStdErr',
+        "$($Git.Path) $($_args -join ' ')"
+    )
+
+    $StackTraceField = [ErrorRecord].GetField('_scriptStackTrace', 'Instance,NonPublic')
+    $StackTraceField.SetValue($ErrorRecord, 'foo')
+    Write-Error -ErrorRecord $ErrorRecord
 }
 Set-Alias git Invoke-Git
