2b9b4a3f0696141071c22c115d395017b326dd18
Freddie Sackur <github@dustyfox.uk>
2022-02-20 15:30:47 +0000

    Aliases are opt-out

--- Victor.psm1
+++ Victor.psm1
@@ -1,10 +1,18 @@
+[CmdletBinding()]
+param
+(
+    [switch]$NoAlias = $env:VICTOR_NO_PS_ALIAS
+)
 
 Get-ChildItem $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue | ForEach-Object {. $_.FullName}
 Get-ChildItem $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue | ForEach-Object {. $_.FullName}
 
-Set-Alias a Add-GitFile
-Set-Alias amend Update-GitCommit
-Set-Alias c Write-GitCommit
-Set-Alias ggl Get-GitLog
-Set-Alias rebase Invoke-GitRebase
-Set-Alias rst Reset-Git
+if (-not $NoAlias)
+{
+    Set-Alias a Victor\Add-GitFile
+    Set-Alias amend Victor\Update-GitCommit
+    Set-Alias c Victor\Write-GitCommit
+    Set-Alias ggl Victor\Get-GitLog
+    Set-Alias rebase Victor\Invoke-GitRebase
+    Set-Alias rst Victor\Reset-Git
+}
